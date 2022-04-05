213af75b63572f81ea1a53bf72abf4a62c517e9c
angcast <angcast@mit.edu>
2022-04-05 04:08:13 -0400

    made isFingerUp and FingersUp function to track which fingers are up

--- gestures.py
+++ gestures.py
@@ -1,7 +1,37 @@
+from enum import Enum
 import cv2
 import mediapipe as mp
 import pyautogui
 
+class Fingers(Enum):
+    THUMB = 0
+    INDEX = 1
+    MIDDLE = 2
+    RING = 3
+    PINKY = 4
+
+class LandMarkPoints(Enum):
+    WRITE = 0
+    THUMB_CMC = 1
+    THUMB_MCP = 2
+    THUMB_IP = 3
+    THUMB_TIP = 4
+    INDEX_FINGER_MCP = 5
+    INDEX_FINGER_PIP = 6
+    INDEX_FINGER_DIP = 7
+    INDEX_FINGER_TIP = 8
+    MIDDLE_FINGER_MCP = 9
+    MIDDLE_FINGER_PIP = 10
+    MIDDLE_FINGER_DIP = 11
+    MIDDLE_FINGER_TIP = 12
+    RING_FINGER_MCP = 13
+    RING_FINGER_PIP = 14
+    RING_FINGER_DIP = 15
+    RING_FINGER_TIP = 16
+    PINKY_MCP = 17
+    PINKY_PIP = 18
+    PINKY_DIP = 19
+    PINKY_TIP = 20
 class handTracker():
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5,modelComplexity=1,trackCon=0.5):
         self.mode = mode
@@ -38,6 +68,38 @@ class handTracker():
 
         return lmlist
 
+    def isFingerUp(self, finger, landmarkList): 
+        if len(landmarkList) != 0: 
+            if finger == Fingers.THUMB: 
+                tip = landmarkList[LandMarkPoints.THUMB_TIP.value][1]
+                ip = landmarkList[LandMarkPoints.THUMB_IP.value][1]
+                return tip > ip
+            if finger == Fingers.INDEX: 
+                pip = landmarkList[LandMarkPoints.INDEX_FINGER_PIP.value][2]
+                tip = landmarkList[LandMarkPoints.INDEX_FINGER_TIP.value][2]
+            elif finger == Fingers.MIDDLE: 
+                pip = landmarkList[LandMarkPoints.MIDDLE_FINGER_PIP.value][2]
+                tip = landmarkList[LandMarkPoints.MIDDLE_FINGER_TIP.value][2]
+            elif finger == Fingers.PINKY:
+                pip = landmarkList[LandMarkPoints.PINKY_PIP.value][2]
+                tip = landmarkList[LandMarkPoints.PINKY_TIP.value][2]
+            elif finger == Fingers.RING: 
+                pip = landmarkList[LandMarkPoints.RING_FINGER_PIP.value][2]
+                tip = landmarkList[LandMarkPoints.RING_FINGER_TIP.value][2]
+            return tip < pip
+        return False
+
+    def fingersUp(self, landmarkList):
+        '''
+            Returns a list of the ids of the fingers that are up
+        '''
+        fingers = []
+        for finger in Fingers: 
+            isUp = self.isFingerUp(finger, landmarkList)
+            if isUp: 
+                fingers.append(finger)
+        return fingers
+
 def main():
     cap = cv2.VideoCapture(0)
     tracker = handTracker()
@@ -46,9 +108,10 @@ def main():
         success,image = cap.read()
         image = tracker.handsFinder(image)
         lmList = tracker.positionFinder(image)
-        if len(lmList) != 0:
-            print(lmList[4])
-
+        tracker.isPointing(lmList)
+        print(tracker.fingersUp(lmList))
+        # if len(lmList) != 0:
+        #     print(lmList[4])
         cv2.imshow("Video",image)
         cv2.waitKey(1)
 
